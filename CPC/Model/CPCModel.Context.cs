4b7825f1929f0caac1ec543425583f0480680f1d
sherikhan <sherikhanx@gmail.com>
2019-11-19 15:30:46 +0500

    done

--- CPC/Model/CPCModel.Context.cs
+++ CPC/Model/CPCModel.Context.cs
@@ -34,10 +34,10 @@ namespace CPC.Model
         public virtual DbSet<CPCAnnexureIDetail> CPCAnnexureIDetails { get; set; }
         public virtual DbSet<CPCAnnexureII> CPCAnnexureIIs { get; set; }
         public virtual DbSet<CPCAnnexureIIDetail> CPCAnnexureIIDetails { get; set; }
-        public virtual DbSet<CPCAnnexureIII> CPCAnnexureIIIs { get; set; }
-        public virtual DbSet<CPCAnnexureIIIDetail> CPCAnnexureIIIDetails { get; set; }
         public virtual DbSet<CPCProject> CPCProjects { get; set; }
         public virtual DbSet<CPCCashInTransit> CPCCashInTransits { get; set; }
         public virtual DbSet<CPCCashInTransitChild> CPCCashInTransitChilds { get; set; }
+        public virtual DbSet<CPCAnnexureIII> CPCAnnexureIIIs { get; set; }
+        public virtual DbSet<CPCAnnexureIIIDetail> CPCAnnexureIIIDetails { get; set; }
     }
 }
